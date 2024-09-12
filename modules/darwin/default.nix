de598c3e1d7bb16d2c95b9ed365965c27fe94df7
Joel DSouza <joeldsouzax@gmail.com>
2024-09-12 11:21:50 +0200

    added linux-builder

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -14,7 +14,22 @@
   # Setup user, packages, programs
   nix = {
     package = pkgs.nixVersions.latest;
-    linux-builder.enable = true;
+    linux-builder = {
+      enable = true;
+      ephemeral = true;
+      maxJobs = 4;
+      config = {
+
+        virtualisation = {
+          darwin-builder = {
+            diskSize = 40 * 1024;
+            memorySize = 8 * 1024;
+          };
+          cores = 6;
+        };
+
+      };
+    };
     settings.trusted-users = [ "@admin" "joel" "joeldsouza" ];
 
     gc = {
